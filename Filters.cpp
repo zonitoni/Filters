8da2e22ed3aa8abc3c5d7aa105cb6e7a2f41b3d4
zonitoni <anton.zellhofer@gmail.com>
2025-10-06 10:16:54 +0200

    Add files via upload

---
+++ Filters.cpp
@@ -0,0 +1,53 @@
+#include "Filters.h"
+#include <math.h>
+FilteredValue::FilteredValue() {
+  a_lpf1 = 0.075;
+  a_lpf2 = 0.025;
+  isActivelpf1 = false;
+  isActivelpf2 = false;
+}
+void FilteredValue::updateSample(float sample) {
+  xn = sample;
+  isActivelpf1? lpf1 = this->LPF1() : lpf1 = 0;
+  isActivelpf2? lpf2 = this->LPF2() : lpf2 = 0;
+}
+
+void FilteredValue::setLPF1Constant(float a){
+  a_lpf1 = a;
+}
+void FilteredValue::setLPF2Constant(float a){
+  a_lpf2 = a;
+}
+void FilteredValue::doLPF1(bool isActive){
+  isActivelpf1 = isActive;
+}
+void FilteredValue::doLPF2(bool isActive){
+  isActivelpf2 = isActive;
+}
+float FilteredValue::LPF1() {
+  float yn = a_lpf1 * xn + (1 - a_lpf1) * this->getOldValueLPF1();
+  this->setOldValueLPF1(yn);
+  return yn;
+}
+float FilteredValue::LPF2() {
+  float yn = a_lpf2 * xn + (a_lpf2) * (1 - a_lpf2) * this->getOldXnLPF2() + (1 - a_lpf2) * (1 - a_lpf2) * this->getOldYnLPF2();
+  this->setOldValuesLPF2(xn, yn);
+  return yn;
+}
+void FilteredValue::setOldValueLPF1(float val) {
+  yn_1_lpf1 = val;
+}
+float FilteredValue::getOldValueLPF1() {
+  return yn_1_lpf1;
+}
+void FilteredValue::setOldValuesLPF2(float xVal, float yVal) {
+  yn_2_lpf2 = yn_1_lpf2;
+  yn_1_lpf2 = yVal;
+  xn_1_lpf2 = xVal;
+}
+float FilteredValue::getOldXnLPF2() {
+  return xn_1_lpf2;
+}
+float FilteredValue::getOldYnLPF2() {
+  return yn_2_lpf2;
+}
---
+++ Filters.h
@@ -0,0 +1,38 @@
+#ifndef FILTERS
+#define FILTERS
+
+
+class FilteredValue {
+public:
+  float xn;
+  float lpf1;
+  float lpf2;
+
+  FilteredValue();
+
+  void setLPF1Constant(float);
+  void setLPF2Constant(float);
+  void updateSample(float);
+  void doLPF1(bool);
+  void doLPF2(bool);
+
+  ~FilteredValue();
+
+private:
+  float yn_1_lpf1;
+  float xn_1_lpf2;
+  float yn_1_lpf2;
+  float yn_2_lpf2;
+  float a_lpf1;
+  float a_lpf2;
+  bool isActivelpf1;
+  bool isActivelpf2;
+  float LPF1();
+  float LPF2();
+  void setOldValueLPF1(float);
+  float getOldValueLPF1();
+  void setOldValuesLPF2(float, float);
+  float getOldXnLPF2();
+  float getOldYnLPF2();
+};
+#endif
